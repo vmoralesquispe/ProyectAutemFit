43cb19d68f7cc99ba7a580584f760a216792f2e4
vmoralesquispe <vmoralesquispe@gmail.com>
2020-06-04 16:54:26 -0500

    Cambios 04/06/20

---
+++ ProjectAutemFit/app/src/main/java/com/example/projectautemfit/Alimentos_Menu.java
@@ -0,0 +1,14 @@
+package com.example.projectautemfit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Alimentos_Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alimentos__menu);
+    }
+}
